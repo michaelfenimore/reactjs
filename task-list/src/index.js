e0cf8df90682e2ec82bf0625474a64e036f8a87d
Michael Fenimore <michael.fenimore@cbs.com>
2017-06-12 16:16:09 -0400

    Add title header and text input field - Start adding a basic structure to the form and list

--- task-list/src/index.js
+++ task-list/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class Title extends React.Component {
+	render() {
+		return (
+			<h1>My Todo List</h1>
+		)
+	}
+}
+
+class Todo extends React.Component {
+	render() {
+		return (
+			<div class="col-md-16">
+				<Title />
+				<input type="text" name="todo-item" />
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+	<Todo />,
+	document.getElementById('main')
+);
+
 registerServiceWorker();
